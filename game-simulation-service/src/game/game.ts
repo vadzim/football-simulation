74a4643075ba37cc49c9c191c7bf732b92241826
Vadzim Zieńka <v@vadzim.info>
2023-03-29 15:58:43 +0200

    implement game simulator

--- game-simulation-service/src/game/game.ts
+++ game-simulation-service/src/game/game.ts
@@ -1,3 +1,24 @@
-export async function* startSimulator(data: unknown) {
-	void data
+import sleep from "sleep-promise"
+
+export async function* startSimulator(
+	data: { id: unknown; team1: string; team2: string },
+	options: { scoreDelay: number } = { scoreDelay: 10_000 },
+) {
+	let score1 = 0
+	let score2 = 0
+
+	const start = Date.now()
+
+	yield { ...data, score1, score2, status: "started" }
+
+	for (let i = 0; i < 9; i++) {
+		await sleep(start + options.scoreDelay * (i + 1) - Date.now())
+		if (Math.random() < 0.5) {
+			score1++
+		} else {
+			score2++
+		}
+		yield { ...data, score1, score2, status: "pending" }
+	}
+	yield { ...data, score1, score2, status: "finished" }
 }
