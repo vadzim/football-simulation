2bd62d848ef80daff045ee53756da52a7fac40aa
Vadzim Zieńka <v@vadzim.info>
2023-03-29 16:02:25 +0200

    fix simulator server implementation

--- game-simulation-service/src/server.ts
+++ game-simulation-service/src/server.ts
@@ -1,17 +1,23 @@
 import net from "net"
 import { Readable } from "stream"
-import { startSimulator } from "./game/game"
+import { from } from "ix/asynciterable"
+import { map } from "ix/asynciterable/operators"
 import getStream from "get-stream"
+import { startSimulator } from "./game/game"
 
-const server = net.createServer({ allowHalfOpen: true }, async socket => {
+export const server = net.createServer({ allowHalfOpen: true }, async socket => {
 	console.log("Client connected:", socket.remoteAddress, socket.remotePort)
 
-	const game = JSON.parse(String(await getStream.buffer(socket)))
+	const game = JSON.parse(String(await getStream.buffer(socket)).trim())
 	console.log("Starting a game:", game)
 
-	Readable.from(startSimulator(game)).pipe(socket)
-	console.log("Game finished:", game)
+	Readable.from(stringifyAsyncIterable(startSimulator(game)))
+		.pipe(socket)
+		.on("finish", () => console.log("Game finished:", game))
 })
+
+const stringifyAsyncIterable = (iterable: AsyncIterable) =>
+	from(iterable).pipe(map(data => JSON.stringify(data) + "\n"))
 
 const port = process.env.PORT || 3000
 
