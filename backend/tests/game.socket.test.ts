bb66b1e75d8d212488894eae54533b06c2c7ef55
Vadzim Zieńka <v@vadzim.info>
2023-03-30 19:35:01 +0200

    fixed backend tests

--- backend/tests/game.socket.test.ts
+++ backend/tests/game.socket.test.ts
@@ -1,12 +1,16 @@
 import request from "supertest"
 import WebSocket from "ws"
 import { app } from "../src/server"
+import { type Server } from "http"
 
-describe("ws://server/games/:id", () => {
-	let server, ws, port
+describe("ws://server/games/listen", () => {
+	let server: Server
+	let ws: WebSocket
+	let port: number
+
 	beforeAll(() => {
 		server = app.listen()
-		;({ port } = server.address())
+		;({ port } = server.address() as { port: number })
 	})
 
 	afterAll(() => {
@@ -14,7 +18,7 @@ describe("ws://server/games/:id", () => {
 	})
 
 	beforeEach(done => {
-		ws = new WebSocket(`ws://localhost:${port}/games/1`)
+		ws = new WebSocket(`ws://localhost:${port}/games/listen`)
 		ws.on("open", () => {
 			done()
 		})
@@ -31,36 +35,36 @@ describe("ws://server/games/:id", () => {
 		{
 			const socketInput = new Promise((resolve, reject) => ws.once("message", resolve).once("error", reject))
 
-			const res = await request(server).post("/games/1/start")
+			const res = await request(server).post("/games/start")
 			expect(res.statusCode).toEqual(200)
 
 			const json = JSON.parse(String(await socketInput))
 
 			expect(json).toEqual({
-				id: "1",
-				team1: "Germany",
-				team2: "Poland",
-				score1: 0,
-				score2: 0,
 				started: true,
+				games: [
+					{ id: "1", team1: "Germany", team2: "Poland", score1: 0, score2: 0 },
+					{ id: "2", team1: "Brazil", team2: "Mexico", score1: 0, score2: 0 },
+					{ id: "3", team1: "Argentina", team2: "Uruguay", score1: 0, score2: 0 },
+				],
 			})
 		}
 
 		{
 			const socketInput = new Promise((resolve, reject) => ws.once("message", resolve).once("error", reject))
 
-			const res = await request(server).post("/games/1/stop")
+			const res = await request(server).post("/games/stop")
 			expect(res.statusCode).toEqual(200)
 
 			const json = JSON.parse(String(await socketInput))
 
 			expect(json).toEqual({
-				id: "1",
-				team1: "Germany",
-				team2: "Poland",
-				score1: 0,
-				score2: 0,
 				started: false,
+				games: [
+					{ id: "1", team1: "Germany", team2: "Poland", score1: 0, score2: 0 },
+					{ id: "2", team1: "Brazil", team2: "Mexico", score1: 0, score2: 0 },
+					{ id: "3", team1: "Argentina", team2: "Uruguay", score1: 0, score2: 0 },
+				],
 			})
 		}
 
