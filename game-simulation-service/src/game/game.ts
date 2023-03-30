0a57c78f1a5af5378c906672513a3a89c181ac7a
Vadzim Zieńka <v@vadzim.info>
2023-03-30 19:34:47 +0200

    fixed simulation process

--- game-simulation-service/src/game/game.ts
+++ game-simulation-service/src/game/game.ts
@@ -1,26 +1,17 @@
 import sleep from "sleep-promise"
 
 export async function* startSimulator(
-	data: { id: unknown; team1: string; team2: string },
+	games: { id: string; team1: string; team2: string }[],
 	options: { scoreDelay: number } = { scoreDelay: 10_000 },
 ) {
-	let score1 = 0
-	let score2 = 0
-
 	const start = Date.now()
-
-	yield { ...data, score1, score2, status: "started" }
-
-	const probability = Math.random()
 
 	for (let i = 0; i < 9; i++) {
 		await sleep(start + options.scoreDelay * (i + 1) - Date.now())
-		if (Math.random() < probability) {
-			score1++
-		} else {
-			score2++
-		}
-		yield { ...data, score1, score2, status: "pending" }
+		const random = Math.random() * games.length
+		const gameIndex = Math.floor(random)
+		const game = games[gameIndex]
+		const scoredTeam = random - gameIndex < 0.5 ? game.team1 : game.team2
+		yield { id: game.id, scoredTeam }
 	}
-	yield { ...data, score1, score2, status: "finished" }
 }
